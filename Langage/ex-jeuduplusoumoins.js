acdfc47a8ec667c02562cc633e8e86db6dc7a995
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2016-06-28 14:43:37 +0200

    Enoncé jeu du plus ou moins

---
+++ Langage/ex-jeuduplusoumoins.js
@@ -0,0 +1,11 @@
+/*
+Exercice jeu du plus ou moins :
+1 - Générer un entier aléatoire entre 0 et 100 (voir Math sur MDN)
+2 - Proposer à l'utilisteur de saisir un entier avec Readline de Node
+et afficher si le nombre est plus grand ou plus petit
+3 - Pouvoir rejouer jusqu'à ce que l'entier soit trouvé
+4 - Gérer les erreurs de saisie (saisie 'ABC'...)
+5 - Stocker les tentatives dans un tableaux et réafficher entre
+chaque tour (Array sur MDN, ressemble à List Java)
+ */
+
---
+++ Langage/objet.js
@@ -0,0 +1,3 @@
+/**
+ * Created by romain on 28/06/2016.
+ */
