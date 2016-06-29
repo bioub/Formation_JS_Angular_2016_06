da9e7fb1585d6e2e61f850deff3d38c3d61a1557
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2016-06-29 14:01:52 +0200

    AngularHello

---
+++ AngularHello/function.js
@@ -0,0 +1,21 @@
+
+
+function ngApp()
+{
+    var title = 'Titre';
+
+    function ngController()
+    {
+        var prenoms = ['Romain', 'Jean', 'Eric'];
+
+        function ngRepeat1() {
+            var prenom = 'Romain';
+        }
+        function ngRepeat2() {
+            var prenom = 'Jean';
+        }
+        function ngRepeat3() {
+            var prenom = 'Eric';
+        }
+    }
+}
